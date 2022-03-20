5324c1ab415adc3816c3f021bdeb28b735fffe2f
Mason Embry <mason@embrycode.com>
2022-03-20 10:37:45 -0500

    Throw in some styling

--- src/index.js
+++ src/index.js
@@ -1,15 +1,51 @@
-// noinspection JSUnusedGlobalSymbols
+// noinspection JSUnusedGlobalSymbols,JSUnresolvedFunction
 
 import * as midicube from 'midicube';
 window.MIDI = midicube;
 
+const DEFAULT_BPM = 90;
+
 const template = document.createElement('template');
 
 template.innerHTML = `
-  <span>
-     <button id="playPause">Play</button>
-     <button id="reset">Reset</button>
+  <span id="controls">
+    <input id="bpm-input" name="tempo" type="number"/>
+    <button id="play-pause">Play</button>
+    <button id="reset">Reset</button>
   </span>
+  
+  <style>
+    * {
+      box-sizing: border-box;
+    }
+    
+    #controls {
+      border: solid 1px lightgray;
+      border-radius: 8px;
+      padding: 8px;
+      
+      display: inline-flex;
+      align-items: center;
+    }
+    
+    input {
+      width: 70px;
+      height: 30px;
+      border: solid 1px lightgray;
+      padding: 0 8px;
+      border-radius: 4px
+    }
+    
+    button {
+      background: transparent;
+      border: solid 1px lightgray;
+      border-radius: 4px;
+      width: 50px;
+      height: 30px;
+      cursor: pointer;
+      margin-left: 4px;
+    }
+  </style>
 `;
 
 class dsMidiPlayer extends HTMLElement {
@@ -31,40 +67,48 @@ class dsMidiPlayer extends HTMLElement {
   }
 
   initPlayer() {
-    const midiFileSrc = this.getAttribute('midi-file-src');
-
     MIDI.loadPlugin({
       soundfontUrl: './soundfont/',
       onerror: (e) => {
         console.error(e);
       },
       onsuccess: () => {
-        this.player = new MIDI.Player();
-
-        this.player.loadFile(
-          midiFileSrc,
-          () => {
-            console.log('onsuccess');
-          },
-          (state, progress) => {
-            console.log('onprogress:', state, progress);
-          },
-          (err) => {
-            console.error('onerror:', err);
-          }
-        );
+        this.setPlayer();
+        this.setBpmAndLoadFile(DEFAULT_BPM);
       },
     });
   }
 
+  setPlayer() {
+    this.player = new MIDI.Player();
+  }
+
+  setBpmAndLoadFile(bpm) {
+    const midiFileSrc = this.getAttribute('midi-file-src');
+
+    this.bpmInput.value = bpm;
+    this.player.BPM = bpm;
+
+    this.player.loadFile(
+      midiFileSrc,
+      () => {}, // onsuccess
+      () => {}, // onprogress
+      (err) => {
+        console.error(err);
+      } // onerror
+    );
+  }
+
   registerElements() {
-    this.playPauseButton = this._shadowRoot.getElementById('playPause');
+    this.playPauseButton = this._shadowRoot.getElementById('play-pause');
     this.resetButton = this._shadowRoot.getElementById('reset');
+    this.bpmInput = this._shadowRoot.getElementById('bpm-input');
   }
 
   registerHandlers() {
     this.playPauseButton.onclick = this.handlePlayPauseClick;
     this.resetButton.onclick = this.handleStopClick;
+    this.bpmInput.onchange = this.handleBpmChange;
   }
 
   handlePlayPauseClick = () => {
@@ -73,6 +117,11 @@ class dsMidiPlayer extends HTMLElement {
 
   handleStopClick = () => {
     this.pausePlayStop(true);
+  };
+
+  handleBpmChange = (e) => {
+    this.pausePlayStop(true);
+    this.setBpmAndLoadFile(e.target.value);
   };
 
   pausePlayStop = (isStop) => {
